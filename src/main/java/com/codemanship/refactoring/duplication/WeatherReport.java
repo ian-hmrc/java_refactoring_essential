d0a872efc996c241d983434d72513fa2a540007b
davide <telapo@users.noreply.github.com>
2026-06-20 12:02:51 +0200

    It works!

--- src/main/java/com/codemanship/refactoring/duplication/WeatherReport.java
+++ src/main/java/com/codemanship/refactoring/duplication/WeatherReport.java
@@ -7,26 +7,7 @@ class WeatherReport {
     void formatDailyReport(List<Forecast> forecasts, List<String> output) {
 
         for (Forecast forecast : forecasts) {
-
-            if (forecast.isMorning()) {
-                String line = forecast.formatForecast(forecast);
-                output.add(line);
-            }
-
-            if (forecast.isAfternoon()) {
-                String line = forecast.formatForecast(forecast);
-                output.add(line);
-            }
-
-            if (forecast.isEvening()) {
-                String line = forecast.formatForecast(forecast);
-                output.add(line);
-            }
-
-            if (forecast.isNight()) {
-                String line = forecast.formatForecast(forecast);
-                output.add(line);
-            }
+            output.add(forecast.formatForecast(forecast));
         }
     }
 
